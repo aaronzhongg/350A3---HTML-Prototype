ee596a2a4e82ed89451c1abe8c3168cb6088f7ff
Aaron Zhong <aaron_zhong2b@hotmail.com>
2016-05-19 09:24:21 +1200

    .

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("sidebar").innerHTML =
+'<ul>'+
+ '<li style="font-size:14pt;"><a href="#"> IJIF <br>HOME AUTOMATION</a></li>'+
+ '<li class="li1"><a href="index.html">Dashboard</a></li>'+
+ '<li class="li2"><a href="doors.html">Doors</a></li>'+
+ '<li class="li3"><a href="windows.html">Windows</a></li>'+
+ '<li class="li4"><a href="lights.html">Lights</a></li>'+
+ '<li class="li5"><a href="#">Temperature</a></li>'+
+ '<li class="li6"><a href="#">Settings</a></li>'+
+'</ul>';
